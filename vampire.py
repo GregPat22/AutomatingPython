9eefb258ec8791114c2473f3c9b74ee7d9a39e68
Greg Patini <gregorio.patini@applied.it>
2025-09-17 16:28:40 +0200

    little test on if and elif syntax

---
+++ vampire.py
@@ -0,0 +1,10 @@
+name = "Dracul"
+age = 3000
+if name == "Dracula":
+    print("Welcome, Count!")
+elif age < 300:
+    print("You are quite young for a vampire.") 
+elif age > 2000:
+    print("You must be an ancient vampire!") 
+elif age > 100:
+    print("You could be just a granny")
